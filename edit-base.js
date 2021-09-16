64e4174ee2aa0be9d8207c6b095f1118033128d8
jsimplicio <hi@julessimplic.io>
2021-09-16 14:27:56 -0500

    Edit script and dump

--- edit-base.js
+++ edit-base.js
@@ -31,7 +31,7 @@ JSON.parse(JSON.stringify(json));
 // }
 
 
-const values = ["00-base-color","01-background-base","02-background-toolbar","03-text-toolbar","03-text-toolbar-secondary-text","03-text-toolbar-toolbar-icon-hover","04-background-content-light","05-border-low-contrast","08-highlight-rows","09-modal-backgrounds","09a-modal-backgrounds","09b-modal-backgrounds-tab-and-search","10-url-details","11-update-alert","12-critical-alert","13-warning-alert","14-information-alert","15-icon-status",
+const values = ["00-base-color","01-background-base","02-background-toolbar","03-text-toolbar","03-text-toolbar-secondary-text","03a-text-toolbar","03a-text-toolbar-toolbar-icon-hover","03a-text-toolbar-secondary-text","03b-text-toolbar","03-text-toolbar-toolbar-icon-hover","03b-text-toolbar-toolbar-icon-hover","03b-text-toolbar-secondary-text","04-background-content-light","05-border-low-contrast","06-border-high-contrast","08-highlight-rows","09-modal-backgrounds","09a-modal-backgrounds","09b-modal-backgrounds-tab-and-search","10-url-details","11-update-alert","12-critical-alert","13-warning-alert","14-information-alert","15-icon-status",
 "15a-icon-status-on-dark-background","15b-icon-status-on-light-background"];
 
 // for ( var i = 0; i < values.length; i++) {
